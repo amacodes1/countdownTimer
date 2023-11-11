954573eeb95175d711a410a7c016a0078dc1bacf
mercyama <mercyezihe2@gmail.com>
2023-11-11 23:56:39 +0100

    commited an update

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const startNumber = 9;
 const flipCard = document.querySelector(".flip-card");
 const topHalf = flipCard.querySelector(".top");
 const bottomHalf = flipCard.querySelector(".bottom");
+const topFlip = document.createElement("div");
+topFlip.classList.add("top-flip");
+const bottomFlip = document.createElement("div");
+bottomFlip.classList.add("bottom-flip");
 
 // [...flipCard.children].forEach((child) => {
 //   child.textContent = startNumber;
