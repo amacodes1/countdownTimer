b7987fcdec34107fa7c4a7905e011a83c3170ea8
mercyama <mercyezihe2@gmail.com>
2023-11-12 00:50:22 +0100

    commited an update

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const startNumber = 9;
 const flipCard = document.querySelector(".flip-card");
-const topHalf = flipCard.querySelector(".top");
-const bottomHalf = flipCard.querySelector(".bottom");
-const topFlip = document.createElement("div");
-topFlip.classList.add("top-flip");
-const bottomFlip = document.createElement("div");
-bottomFlip.classList.add("bottom-flip");
 
-// [...flipCard.children].forEach((child) => {
-//   child.textContent = startNumber;
-// });
+flip(flipCard);
 
-top.textContent = startNumber;
-bottomHalf.textContent = startNumber;
+function flip(flipCard) {
+  const topHalf = flipCard.querySelector(".top");
+  const bottomHalf = flipCard.querySelector(".bottom");
+  const topFlip = document.createElement("div");
+  topFlip.classList.add("top-flip");
+  const bottomFlip = document.createElement("div");
+  bottomFlip.classList.add("bottom-flip");
+  const startNumber = parseInt(topHalf.textContent);
 
-flipCard.dataset.currentNumber = startNumber;
-flipCard.dataset.nextNumber = startNumber - 1;
-flipCard.addEventListener("animationstart", (e) => {
-  topHalf.textContent = startNumber - 1;
-});
-flipCard.addEventListener("animationend", (e) => {
-  bottomHalf.textContent = startNumber - 1;
-});
-flipCard.classList.add("flip");
+  top.textContent = startNumber;
+  bottomHalf.textContent = startNumber;
+  topFlip.textContent = startNumber;
+  bottomFlip.textContent = startNumber - 1;
+
+  topFlip.addEventListener("animationstart", (e) => {
+    topHalf.textContent = startNumber - 1;
+  });
+  topFlip.addEventListener("animationend", (e) => {
+    topFlip.remove();
+  });
+  bottomFlip.addEventListener("animationend", (e) => {
+    bottomHalf.textContent = startNumber - 1;
+    bottomFlip.remove();
+    flip(flipCard);
+  });
+  flipCard.append(topFlip, bottomFlip);
+}
